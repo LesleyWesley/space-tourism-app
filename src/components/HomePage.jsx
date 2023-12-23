667a6210df4d5da53eec609704c59cefc9969f49
lesley_w <lrwesley92@gmail.com>
2023-12-23 12:02:38 -0600

    Added homepage and Header component files

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./Header.jsx"
+
+function HomePage() {
+  return (
+      <div>
+        <h1>This is the homepage!</h1>
+        <Header />
+      </div>
+  )
+}
+
+export default HomePage
