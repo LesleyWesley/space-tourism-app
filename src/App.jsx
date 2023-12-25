331832679b84fbe189693def4fb843474eac872a
lesley_w <lrwesley92@gmail.com>
2023-12-25 17:52:56 -0600

    Installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { NavLink, Route } from "react-router-dom"
 import HomePage from "../src/pages/HomePage.jsx"
 import DestinationPage from "../src/pages/DestinationPage.jsx"
 import CrewPage from "../src/pages/CrewPage.jsx"
@@ -10,11 +11,7 @@ import TechnologyPage from "../src/pages/TechnologyPage.jsx"
 function App() {
   return (
     <div>
-      <h1>This is the app, baby!</h1>
       <HomePage />
-      <DestinationPage />
-      <CrewPage />
-      <TechnologyPage />
     </div>
   )
 }
