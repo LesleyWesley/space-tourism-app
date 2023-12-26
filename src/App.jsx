9a4ddea89aa6d025aee0dd0fae3467bff502bbb2
lesley_w <lrwesley92@gmail.com>
2023-12-26 12:35:53 -0600

    Added base stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { NavLink, Route } from "react-router-dom"
 import HomePage from "../src/pages/HomePage.jsx"
 import DestinationPage from "../src/pages/DestinationPage.jsx"
 import CrewPage from "../src/pages/CrewPage.jsx"
@@ -12,6 +10,9 @@ function App() {
   return (
     <div>
       <HomePage />
+      <DestinationPage />
+      <CrewPage />
+      <TechnologyPage />
     </div>
   )
 }
